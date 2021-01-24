a3baf6ddc728b7afb9637007afb73861c1f978cb
Manish Sharma <manish.sharma@bcone.com>
2021-01-24 18:59:48 +0530

    add delete,update

--- src/main/java/com/example/redo/TopicController.java
+++ src/main/java/com/example/redo/TopicController.java
@@ -50,31 +50,50 @@ public class TopicController {
 //        return new ResponseEntity<>(data,headers, HttpStatus.ACCEPTED);
 //    }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/addtopic")
-    public String addtopic(@RequestBody Topic topic, HttpServletRequest req, HttpServletResponse res) throws IOException {
+//    @RequestMapping(method = RequestMethod.POST, value = "/addtopic")
+//    public String addtopic(@RequestBody Topic topic, HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        topicServices.generatetopics(topic);
+//
+//        // can set ur own headers
+//        res.setHeader("ACCEPTED", "success!!");
+//        res.setDateHeader("Curr_time", 1742);
+//        res.setHeader("useless", "Test");
+//        res.setStatus(203);
+//        // traverse all headers names
+////        Collection<String> result = res.getHeaderNames();
+////        List<String> list = result.stream().collect(Collectors.toList());
+////        System.out.println(list);
+////        byte arr[] = req.getInputStream().readAllBytes();
+////        String str=req.getReader().readLine();// can't do as @Requestbody already did that internally
+////        for (byte x : arr) {
+////            char ch = (char) (x & 0xFF);
+////            System.out.print(ch);
+////        }
+//        return "done";
+//    }
+    // add topic
+    @RequestMapping(method = RequestMethod.POST,value = "/addtopic")
+    public void addtopic(@RequestBody Topic topic)
+    {
         topicServices.generatetopics(topic);
-
-        // can set ur own headers
-        res.setHeader("ACCEPTED", "success!!");
-        res.setDateHeader("Curr_time", 1742);
-        res.setHeader("useless", "Test");
-        res.setStatus(203);
-        // traverse all headers names
-//        Collection<String> result = res.getHeaderNames();
-//        List<String> list = result.stream().collect(Collectors.toList());
-//        System.out.println(list);
-//        byte arr[] = req.getInputStream().readAllBytes();
-//        String str=req.getReader().readLine();// can't do as @Requestbody already did that internally
-//        for (byte x : arr) {
-//            char ch = (char) (x & 0xFF);
-//            System.out.print(ch);
-//        }
-        return req.getContextPath();
     }
 
     // get topics by id
     @RequestMapping(method = RequestMethod.GET, value = "/topic/{id}")
     public void topicbyId(@PathVariable String id) {
         topicServices.gettopicById(id);
+    }
+
+    // update topic
+    @RequestMapping(method = RequestMethod.PATCH,value = "/updatetopic/{id}")
+    public void updatetopic(@RequestBody Topic topic,@PathVariable String myid)
+    {
+         topicServices.updatetopic(myid,topic);
+    }
+// delete the topic
+    @RequestMapping(method = RequestMethod.DELETE,value = "/deletetopic/{id}")
+    public void deletetopic(@RequestBody Topic topic, @PathVariable String delid)
+    {
+        topicServices.deletetopic(delid,topic);
     }
 }
--- src/main/java/com/example/redo/TopicServices.java
+++ src/main/java/com/example/redo/TopicServices.java
@@ -10,15 +10,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class TopicServices {
 
-   private List<Topic> mytopics=new ArrayList<>(Arrays.asList(
-           new Topic("12","abc","dd"),
-           new Topic("13","acd","ee"),
-           new Topic("14","cvb","aa")
-   ));
+    private List<Topic> mytopics = new ArrayList<>(Arrays.asList(
+            new Topic("12", "abc", "dd"),
+            new Topic("13", "acd", "ee"),
+            new Topic("14", "cvb", "aa")
+    ));
 
     public void generatetopics(Topic topic) {
         mytopics.add(topic);
@@ -30,13 +31,39 @@ public class TopicServices {
 
     public Topic gettopicById(String id) {
 
-    List<Topic> topics= mytopics.stream().filter((topic)->{
+        List<Topic> topics = mytopics.stream().filter((topic) -> {
 
-            return topic.getId()==id;
+            return topic.getId() == id;
 
         }).collect(Collectors.toList());
 
-    return topics.get(0);
+        return topics.get(0);
+
+    }
+
+    public void updatetopic(String myid, Topic topic) {
+        if (topic != null) {
+            getalltopics().stream().map((t) -> {
+                if (t.getId() == myid) {
+                    mytopics.add(Integer.parseInt(t.getId()), topic);
+                }
+                return null;
+            });
+
+        } else
+            return;
+    }
+    public void deletetopic(String delid, Topic topic) {
+        if (topic != null) {
+            getalltopics().stream().map((t) -> {
+                if (t.getId() == delid) {
+                    mytopics.remove(Integer.parseInt(delid));
+                }
+                return null;
+            });
+
+        } else
+            return;
 
     }
 }
